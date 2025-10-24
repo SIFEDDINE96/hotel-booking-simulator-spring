b3b2e532852cf7b29547a9cb4cffdebf1ea9d84f
sifeddine <sifeddine.karroum96@gmail.com>
2025-10-24 17:27:54 +0100

    First app commit

---
+++ src/main/java/com/sifeware/hotel_booking_simulator/HotelBookingSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.sifeware.hotel_booking_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingSimulatorApplication.class, args);
+	}
+
+}
